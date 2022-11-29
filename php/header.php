d0bceeb939f9500a59d870df849b3b0c2d1edc27
Mat <118286311+unknws@users.noreply.github.com>
2022-11-29 15:50:25 +0100

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,18 +1,5 @@
 <?php
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <link rel="stylesheet" href="page_journal.css">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/page_des_emissions.css">
-    <link rel="stylesheet" href="../css/page_journal.css">
-
-
 <header class = "header">
     <img src="../../img/Logo.png" alt="Logo" class="logo">
     <div class="headright">

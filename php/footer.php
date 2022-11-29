e1349aff0daae0c40f73e7059320da5ce2f482a6
Ben7821 <117637511+ben7821@users.noreply.github.com>
2022-11-29 20:53:39 +0100

    Add files via upload

--- php/footer.php
+++ php/footer.php
@@ -1,13 +1,17 @@
+<?php
+?>
 <footer>
-	<img src="../img/headerlogo.png" alt="Logo" class="logo">
-	
-	<table>
-		<th><h1>A propos</h1></th>
-		
-		<tr>
-			<td> - Contact </td>
-			<td> - Plan du site </td>
-			<td> - Mentions légales </td>
-		</tr>
-	</table>
-</footer>	
+        <img src="../../img/Logo.png" alt="Logo" class="logo-footer">
+        <h4>A propos</h4>
+        <div class="footer-element">
+            <ul>
+                <li><a href="">- Contact</a></li>
+                <li><a href="">- Plan du site</a></li>
+                <li><a href="">- Mentions Legales</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
+<?php
+?>

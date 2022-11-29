beeea4e87be54b868d7d9f663b40ec74aaba9f68
Ben7821 <117637511+ben7821@users.noreply.github.com>
2022-11-29 14:57:48 +0100

    Add files via upload

---
+++ php/article.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<a class="article border-1" href="">
+    <div class="article-img"><img src="" alt="Image" width="100%" height="100%"></div>
+    <div class="article-container ">
+        <h3>$titre_article</h3>
+        <p>$para_article</p>
+    </div>
+</a>
+
---
+++ php/header.php
@@ -0,0 +1,44 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="page_journal.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/page_des_emissions.css">
+    <link rel="stylesheet" href="../css/page_journal.css">
+
+
+<header class = "header">
+    <img src="../../img/Logo.png" alt="Logo" class="logo">
+    <div class="headright">
+        <a href=""><img src="html css/Menu.png" alt="Menu"></a>
+        <div class="dropdown" id="emission">
+            <a href="" id="btn">EMISSIONS</a>
+            <div class="dropdown-content">
+                <a href="">Le quiz en série</a>
+                <a href="">Histoire de poche</a>
+                <a href="">Le journal</a>
+                <a href="">Debat Lycéen</a>
+                <a href="">1 thème, 3 chansons</a>
+            </div>
+        </div>
+        <div class="dropdown" id="equipe">
+            <a href="" id="btn">JOURNAL</a>
+            <div class="dropdown-content">
+                <a href="">le Web Radio</a>
+                <a href="">Le lycée</a>
+            </div>
+        </div>
+        <div class="dropdown" id="journal">
+            <a href="" id="btn">L'EQUIPE</a>
+            <div class="dropdown-content">
+                <a href=""> . </a>
+            </div>
+        </div>
+    </div>
+</header>

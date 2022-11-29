1a40d164c45cb5e881c5d49694ea56d84bfb7eeb
Ben7821 <117637511+ben7821@users.noreply.github.com>
2022-11-29 14:21:24 +0100

    Add files via upload

---
+++ css/php/page_des_emissions.php
@@ -0,0 +1,91 @@
+<?php
+?>
+<title>Webradio-des-emissions</tittle>
+</head>
+<body>
+
+    <?php require "header.php";?>
+
+    <h1 class="header-page">Les Émissions</h1>
+    <hr size=5 width="93%"color = black>
+
+        <!--First left container, blue color-->
+        <div class="container-left">
+            <a href="" class="box-left border-2">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-left">
+            </a>
+
+            <article>
+                <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+        </div>
+
+        <!--First right container, yellow color-->
+        <div class="container-right">
+            <article>
+                <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+            <a href="" class="box-right border-3">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-right">
+            </a>
+        </div>
+
+        <!--Second left container, rose color-->
+        <div class="container-left">
+            <a href="" class="box-left border-1">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-left">
+            </a>
+
+            <article>
+                <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+        </div>
+
+
+        <!--Second right container, blue color-->
+        <div class="container-right">
+            <article>
+                <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+            <a href="" class="box-right border-2">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-right">
+            </a>
+        </div>
+
+        <!--Third left container, yellow color-->
+        <div class="container-left">
+            <a href="" class="box-left border-3">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-left">
+            </a>
+
+            <article>
+                    <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+        </div>
+
+        <!--Third right container, rose color-->
+        <div class="container-right">
+            <article>
+                <h3>Lorem ipsum </h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolorum officia laboriosam placeat perspiciatis obcaecati id rem error corporis facere ipsa aliquam quos ratione a autem consectetur magnam cumque eveniet.</p>
+            </article>
+            <a href="" class="box-right border-2">
+                <h2>Titre</h2>
+                <img src="../../img/test.png" alt="image" class="img-right">
+            </a>
+        </div>
+
+    <?php require "footer.php";?>
+
+</body>
+</html>

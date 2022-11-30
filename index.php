39ed6d97d421d04e10b19e2c0cb4d71cbf8b00ee
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-11-30 14:39:34 +0100

    ss

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Old Page</title>
+     <meta charset="UTF-8" />
+     <!-- <meta http-equiv="refresh" content="1; URL=https://poupli.net/WebRadio-Guy-Chauvet/template/index.html" /> -->
+   </head>
+   <body>
+   <?php require './php/header.php' ?>
+
+   Content
+
+   <?php include "./php/footer.php" ?>
+</body>
+</html>

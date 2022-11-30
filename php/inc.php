5f37b43d746dd6c3af081381e2eaa1db0964fe3a
Mat <118286311+unknws@users.noreply.github.com>
2022-11-30 16:49:17 +0100

    Create inc.php

---
+++ php/inc.php
@@ -0,0 +1,69 @@
+<?php
+function header() {
+?>
+<header class = "header">
+    <img src="../img/Logo.png" alt="Logo" class="logo">
+    <div class="headright">
+        <a href=""><img src="../img/Menu.png" alt="Menu"></a>
+        <div class="dropdown" id="emission">
+            <a href="" id="btn">EMISSIONS</a>
+            <div class="dropdown-content">
+                <a href="">Le quiz en série</a>
+                <a href="">Histoire de poche</a>
+                <a href="">Le journal</a>
+                <a href="">Debat Lycéen</a>
+                <a href="">1 thème, 3 chansons</a>
+            </div>
+        </div>
+        <div class="dropdown" id="equipe">
+            <a href="" id="btn">JOURNAL</a>
+            <div class="dropdown-content">
+                <a href="">le Web Radio</a>
+                <a href="">Le lycée</a>
+            </div>
+        </div>
+        <div class="dropdown" id="journal">
+            <a href="" id="btn">L'EQUIPE</a>
+            <div class="dropdown-content">
+                <a href=""> . </a>
+            </div>
+        </div>
+    </div>
+</header>
+<?php
+}
+function footer(){
+?>
+<footer>
+        <img src="../img/Logo.png" alt="Logo" class="logo-footer">
+        <h4>A propos</h4>
+        <div class="footer-element">
+            <ul>
+                <li><a href="">- Contact</a></li>
+                <li><a href="">- Plan du site</a></li>
+                <li><a href="">- Mentions Legales</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
+<?php
+?>
+<?php
+  }
+function entete(){
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="page_journal.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/page_des_emissions.css">
+    <link rel="stylesheet" href="../css/page_journal.css">
+</head> 
+<?php
+  }
+ ?>

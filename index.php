c621927c720904adbbd81a9bc40796336dde8d84
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-11-30 14:47:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
      <!-- <meta http-equiv="refresh" content="1; URL=https://poupli.net/WebRadio-Guy-Chauvet/template/index.html" /> -->
    </head>
    <body>
-   <?php require './template/header.html' ?>
+   <?php include './template/header.html' ?>
 
-   <?php require './template/index.html' ?>
+   <?php include './template/index.html' ?>
 
    <?php include "./template/footer.html" ?>
 </body>

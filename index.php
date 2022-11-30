61d834e3eff411349e78d9f8f6ac34c32f55eddb
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-11-30 14:14:57 +0100

    ss

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="main.css">
     <link rel="import" href="/main.html">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
+    <meta http-equiv="refresh" content="delay_time; URL=/template/page_d_acueil.html" />
 </head>
 
 <body>

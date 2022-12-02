f6f832a5f50dd1141feb9ab2954ab27ae9ffe593
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-12-02 08:18:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WebRadio LGC</title>
+     <meta charset="UTF-8" />
+   </head>
+   <body>
+   <?php require './body/header.php' ?>
+
+   <?php require './body/index.php' ?>
+
+   <?php require "./body/footer.php" ?>
+</body>
+</html>

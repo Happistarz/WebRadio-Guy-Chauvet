0a680f4fa16d7272bdfb7ae0ec27461b9544beed
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-02 09:22:06 +0100

    Update inc.php

--- body/inc.php
+++ body/inc.php
@@ -90,9 +90,6 @@ function footer(){
   }
 function entete(){
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="page_journal.css">
@@ -100,7 +97,6 @@ function entete(){
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/page_des_emissions.css">
     <link rel="stylesheet" href="../css/page_journal.css">
-</head> 
 <?php
   }
  ?>

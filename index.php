26189a5a292386f8e99041ef74e9804263cce40f
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-02 09:04:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,6 @@
    <?php require './body/header.html' ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="page_journal.css">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/page_des_emissions.css">
-    <link rel="stylesheet" href="../css/page_journal.css">
     </head>
     <body>
 

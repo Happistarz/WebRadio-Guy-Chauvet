524010367c590695ad487763f3aed0024e2e3149
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-12-02 09:12:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,14 @@
     <head>
         <title>WebRadio LGC</title>
      <meta charset="UTF-8" />
-        
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    </head>
-    <body>
-    <?php require './body/header.html' ?>
-    <link rel="stylesheet" href="./css/header.css">
-<!--   <?php require "./body/entete.php" ?> -->
-
-    <?php include './body/index.php'  ?>
-    <?php include './body/footer.php' ?>
-    </body>
+   </head>
+   <body>
+   <?php require "./body/inc.php" 
+    entete.php;   
+    header();
+       ?>
+   <?php require './body/index.php' ?>
+    
+   <?php include './body/footer.html';?>
+</body>
 </html>

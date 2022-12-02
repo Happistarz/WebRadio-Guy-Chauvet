cb2a0bd2c537fa86938c829ad5c61f7707b672d3
Mat <118286311+unknws@users.noreply.github.com>
2022-12-02 11:35:15 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>WebRadio LGC</title>
-     <meta charset="UTF-8" />
-   </head>
+<?php
+entete();?>
+
+<title>WebRadio LGC</title>
+   
    <?php
     ini_set('display_errors','1');
      require('./php/inc.php');
     echo"<body>";
-    entete();
+  
      eader();
      footer();
      ?>

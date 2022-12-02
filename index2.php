835f75a90a8891bab0d5a30a3cca65700d994247
Mat <118286311+unknws@users.noreply.github.com>
2022-12-02 11:03:46 +0100

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WebRadio LGC</title>
+     <meta charset="UTF-8" />
+   </head>
+   <body>
+
+   <?php require './body/inc.php' 
+     header();
+     footer();
+     ?>
+  
+</body>
+</html>

53d301d3086b6ffca6ec5477868cacb785823080
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-02 09:24:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     <head>
         <title>WebRadio LGC</title>
      <meta charset="UTF-8" />
+     <?php include "./body/inc.php"; entete(); ?>
    </head>
    <body>
-   <?php require "./body/inc.php" 
-    entete();   
+   <?php require "./body/inc.php";
     header();
        ?>
-   <?php require './body/acceuil.html' ?>
+   <?php require './body/acceuil.html'; ?>
     
    <?php include './body/footer.html';?>
 </body>

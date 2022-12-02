3418eceb52e0a25e3729d4312feae646c57c9a89
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-12-02 08:33:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
      <meta charset="UTF-8" />
         
    <?php require './body/header.php' ?>
-   <?php require './body/entete.php' ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="page_journal.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/page_des_emissions.css">
+    <link rel="stylesheet" href="../css/page_journal.css">
    </head>
    <body>
    <?php require './body/header.php' ?>

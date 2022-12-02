d6d47d3273cf35a9a2ad45996b7adb8e2815bdeb
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-02 09:47:02 +0100

    t

--- body/inc.php
+++ body/inc.php
@@ -73,22 +73,23 @@ function header() {
 ";
 }
 function footer(){
-?>
+
+
+echo "
 <footer>
-        <img src="../img/Logo.png" alt="Logo" class="logo-footer">
+        <img src=\"../img/Logo.png\" alt=\"Logo\" class=\"logo-footer\">
         <h4>A propos</h4>
-        <div class="footer-element">
+        <div class=\"footer-element\">
             <ul>
-                <li><a href="">- Contact</a></li>
-                <li><a href="">- Plan du site</a></li>
-                <li><a href="">- Mentions Legales</a></li>
+                <li><a href=\"\">- Contact</a></li>
+                <li><a href=\"\">- Plan du site</a></li>
+                <li><a href=\"\">- Mentions Legales</a></li>
             </ul>
         </div>
     </footer>
-<?php
-?>
-<?php
+    ";
   }
+  
 function entete(){
 
     echo "

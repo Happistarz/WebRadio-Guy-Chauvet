fa12b5fe7c193835c5fda699b6068c60407b8a66
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-07 14:03:59 +0100

    Update entete.php

--- php/entete.php
+++ php/entete.php
@@ -1,4 +1,3 @@
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="page_journal.css">
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/header.css">

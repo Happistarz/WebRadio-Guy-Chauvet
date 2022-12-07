27acae37b9cd64d5229a19ac9db972fea088f617
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-07 15:20:57 +0100

    Update entete.php

--- body/entete.php
+++ body/entete.php
@@ -1,6 +1,6 @@
-  <!--<link rel="stylesheet" href="./css/footer.css">-->
+    <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/reset.css">
- <!--<link rel="stylesheet" href="./css/header.css">-->
-    <!--<link rel="stylesheet" href="./css/page_journal.css">-->
+    <link rel="stylesheet" href="./css/header.css">-->
+    <link rel="stylesheet" href="./css/page_journal.css">
     <link rel="stylesheet" href="./css/page_d_acueil.css">
     <link rel="stylesheet" href="./css/page_des_emissions.css">

f61990d99aa291ab9dd51d67828f899254e9e02f
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-07 14:15:56 +0100

    Update page_des_emissions.php

--- body/page_des_emissions.php
+++ body/page_des_emissions.php
@@ -6,8 +6,8 @@
 
     <meta name=’viewport’ content=’width=device-width, initial-scale=1.0>
     <link rel="stylesheet" href="css/page_des_emissions.css">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
 
     <title>webradio-des-emissions</title>
 </head>

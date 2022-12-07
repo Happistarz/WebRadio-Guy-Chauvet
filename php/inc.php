5253887f90adcfd83922c9365f4cca0945992625
Mat <118286311+unknws@users.noreply.github.com>
2022-12-07 16:24:14 +0100

    Update inc.php

--- php/inc.php
+++ php/inc.php
@@ -39,9 +39,9 @@ function footer(){
         <h1>A propos</h1>
         <div class="footer-element">
             <ul>
-                <li><a href="">- Contact</a></li>
-                <li><a href="">- Plan du site</a></li>
-                <li><a href="">- Mentions Legales</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Plan du site</a></li>
+                <li><a href="">Mentions Legales</a></li>
             </ul>
         </div>
     </footer>

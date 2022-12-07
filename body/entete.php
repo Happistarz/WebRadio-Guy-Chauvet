31a4146e89b4b1bca36ad77164d404b7180c535a
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-07 14:32:35 +0100

    Update entete.php

--- body/entete.php
+++ body/entete.php
@@ -1,5 +1,3 @@
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/page_des_emissions.css">

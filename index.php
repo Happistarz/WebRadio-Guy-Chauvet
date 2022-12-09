73be6c599747a72ebe302ed7c698091c00983188
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-09 09:46:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php ini_set('display_errors', 0); ?>
+        <?php ini_set('display_errors', '0'); ?>
       <title>WebRadio LGC</title>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">

dc8fd44ba7e9e38105d59262f101ac5f592a7231
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-12-09 09:16:44 +0100

    Add files via upload

---
+++ page/index - Copie (3).php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>WebRadio LGC</title>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/x-icon" href="./img/favico.png">
+      <?php require "./body/entete.php" ?>
+   </head>
+   <body>
+
+   <?php require './body/header.php' ?>
+
+   <!-- <?php require './body/index.php' ?> -->
+   <?php require "./template/index_d_acueil.html" ?>
+   <?php require "./body/footer.php" ?>
+
+</body>
+</html>

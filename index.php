e3b118d59f9f7254c10f050bb7ff4625da01a0a4
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-09 08:44:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
       <title>WebRadio LGC</title>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="icon" type="image/x-icon" href="./img/favicon.png">
+        <link rel="icon" type="image/x-icon" href="./img/favico.png">
       <?php require "./body/entete.php" ?>
    </head>
    <body>

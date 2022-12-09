3b3e09dd1845b46f02a77c93d60cd79904c1ac6b
Atis <94861317+AtisFPS@users.noreply.github.com>
2022-12-09 09:28:36 +0100

    Update index-journal.php

--- page/index-journal.php
+++ page/index-journal.php
@@ -5,15 +5,15 @@
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="icon" type="image/x-icon" href="./img/favico.png">
-      <?php require "./body/entete.php" ?>
+      <?php require "../body/entete.php" ?>
    </head>
    <body>
 
-   <?php require './body/header.php' ?>
+   <?php require '../body/header.php' ?>
 
-   <!-- <?php require './body/index.php' ?> -->
-   <?php require "./template/index_journal.html" ?>
-   <?php require "./body/footer.php" ?>
+   <!-- <?php require '../body/index.php' ?> -->
+   <?php require "../template/index_journal.html" ?>
+   <?php require "../body/footer.php" ?>
 
 </body>
 </html>

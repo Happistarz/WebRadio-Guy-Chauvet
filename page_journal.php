f2a0b06811ec2a72d830b07374a5d7bf2d4d6813
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-13 15:32:14 +0100

    Create page_journal.php

---
+++ page_journal.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>WebRadio LGC</title>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" type="image/x-icon" href="./img/favico.png">
+      <?php require './body/entete.php' ?>
+   </head>
+   <body>
+
+   <?php require './body/header.php' ?>
+
+   <!-- <?php require "./body/index.php" ?> -->
+   <?php require "./template/index_journal.html" ?>
+   <?php require "./body/footer.php" ?>
+
+</body>
+</html>

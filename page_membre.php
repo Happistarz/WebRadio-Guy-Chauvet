9caea1be965241baac24fba2c558026a98350f7f
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-16 09:14:02 +0100

    Create page_membre.php

---
+++ page_membre.php
@@ -0,0 +1,18 @@
+
+<html>
+  <head>
+      <title>Lycee LGC</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="icon" type="image/x-icon" href="./img/favico.png">
+      <?php require "./body/entete.php"; ?>
+  </head>
+  <body>
+    <?php require "./body/loader.php"; ?>
+    <?php require "./body/header.php"; ?>
+    
+    <?php require "./template/index_membre.html"; ?>
+    <?php require "./body/footer.php"; ?>
+  </body>
+</html>

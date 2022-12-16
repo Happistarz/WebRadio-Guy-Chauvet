e63e7bb550ff98d381fc14eb5f930d5cf311ca9f
Happiz <63449665+Happistarz@users.noreply.github.com>
2022-12-16 08:43:43 +0100

    Update page_lycee.php

--- page_lycee.php
+++ page_lycee.php
@@ -1,5 +1,10 @@
 <html>
   <head>
+      <title>L'équipe LGC</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="icon" type="image/x-icon" href="./img/favico.png">
     <?php require "./body/entete.php"; ?>
   </head>
   <body>

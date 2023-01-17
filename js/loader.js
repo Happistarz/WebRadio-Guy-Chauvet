71b4b1ecf5ccd1f07a9b967f8ee5f0cb9038263e
Ben7821 <117637511+ben7821@users.noreply.github.com>
2023-01-17 17:15:07 +0100

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,41 @@
+tmp = true;
+// responsive effect 
+function MenuOpen() {
+    console.log(window.innerWidth);
+
+    // condition responsive with window's width
+    if (window.innerWidth < 400) {
+        if (tmp == true) {
+            const btn = document.querySelectorAll("#btn");
+            // display all header buttons
+            btn.forEach(btn => {
+                btn.style.display = "block";
+            });
+
+            tmp = false;
+        } else {
+            tmp = true;
+            const btn = document.querySelectorAll("#btn");
+            // display off all header buttons
+            btn.forEach(btn => {
+                btn.style.display = "none";
+            });
+        }
+    } else {
+        const btn = document.querySelectorAll("#btn");
+        btn.forEach(btn => {
+            btn.style.display = "block";
+        });
+
+    }
+}
+
+// loader event 
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    setTimeout(function() {
+        // animation loader fill up
+        document.getElementById("loader").style.top = "-100vh";
+        console.log("test");
+    }, 500)
+});
